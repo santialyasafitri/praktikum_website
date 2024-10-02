33962acf373dcf3d0675684a26a61b73bdb11b51
santialyasafitri <alyaaachan275@gmail.com>
2024-10-02 14:58:07 +0700

    Update isi.js

--- isi.js
+++ isi.js
@@ -1,7 +1,7 @@
-document.writeIn("<center>");
-document.writeIn("<hr width=600 color=Black>");
-document.writeIn("<h1>UNIVERSITAS LAMPUNG</h1>");
-document.writeIn("<h2>PENDIDIKAN TEKNOLOGI INFORMASI</h2>");
-document.writeIn("<h3>JURUSAN PENDIDIKAN MATEMATIKA DAN ILMU PENGETAHUAN ALAM</h3>");
-document.writeIn("<hr widht=600 color=Black>");
-document.writeIn("</center>");                 
+document.writeln("<center>");
+document.writeln("<hr width=600 color=Black>");
+document.writeln("<h1>UNIVERSITAS LAMPUNG</h1>");
+document.writeln("<h2>PENDIDIKAN TEKNOLOGI INFORMASI</h2>");
+document.writeln("<h3>JURUSAN PENDIDIKAN MATEMATIKA DAN ILMU PENGETAHUAN ALAM</h3>");
+document.writeln("<hr widht=600 color=Black>");
+document.writeln("</center>");                 
